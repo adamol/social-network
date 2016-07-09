af571e7e2f21cb6f622930f66784e2e6c839900d
adamol <adamol1992@gmail.com>
2016-07-10 00:50:15 +0200

    Show friends in profile page

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,6 +6,13 @@
 		<hr>
 	</div>
 	<div class="col-sm-4 col-sm-offset-3">
-		friends and friend requests
+		<h4>{{ $user->getFirstNameOrUsername() }}'s friends</h4>
+		@if (!$user->friends()->count())
+			<p>{{ $user->getFirstNameOrUsername() }} has no friends.</p>
+		@else
+			@foreach ($user->friends() as $user)
+				@include('user/partials/userblock')
+			@endforeach
+		@endif
 	</div>
 @stop
