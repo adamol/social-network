3c3bfb752686f3f377b48cda96bd195d26476afe
adamol <adamol1992@gmail.com>
2016-07-09 23:26:57 +0200

    Add middleware for guest and auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,23 +10,27 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/', [
 	'as' => 'home', 'uses' => 'HomeController@index'
 ]);
-Route::get('/alert', function() {
-	return Redirect::to('/')->with('info', 'Thanks for signing up!');
+
+
+Route::group(['middleware' => 'guest'], function() {
+
+	Route::get('/signup', [
+		'as' => 'auth.signup', 'uses' => 'AuthController@getSignup'
+	]);
+	Route::post('/signup', 'AuthController@postSignup');
+
+	Route::get('/signin', [
+		'as' => 'auth.signin', 'uses' => 'AuthController@getSignin'
+	]);
+	Route::post('/signin', 'AuthController@postSignin');
+
 });
-Route::get('/signup', [
-	'as' => 'auth.signup', 'uses' => 'AuthController@getSignup'
-]);
-Route::post('/signup', 'AuthController@postSignup');
 
-Route::get('/signin', [
-	'as' => 'auth.signin', 'uses' => 'AuthController@getSignin'
-]);
-Route::post('/signin', 'AuthController@postSignin');
-
-Route::get('/signout', [
-	'as'=> 'auth.signout', 'uses' => 'AuthController@signout'
-]);
+Route::group(['middleware' => 'auth'], function() {
+	Route::get('/signout', [
+		'as'=> 'auth.signout', 'uses' => 'AuthController@signout'
+	]);
+});
