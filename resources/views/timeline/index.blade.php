5324439893a3a6e12105faf47b6d71f48ab01cb3
adamol <adamol1992@gmail.com>
2016-07-10 12:00:16 +0200

    Show replies to statuses

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -33,7 +33,7 @@
 							<img src="{{ $status->user->getAvatarUrl() }}" alt="{{ $status->user->getNameOrUsername() }}" class="media-object">
 						</a>
 						<div class="media-body">
-							<h4 class="media-heading"><a href="">Dayle</a></h4>
+							<h4 class="media-heading"><a href="{{ route('profile.index', ['username' => $status->user->username]) }}">{{ $status->user->getNameOrUsername()}} </a></h4>
 							<p>{{ $status->body }}</p>
 							<ul class="list-inline">
 								<li>{{ $status->created_at->diffForHumans() }}</li>
@@ -41,20 +41,22 @@
 								<li>10 likes</li>
 							</ul>
 
-							<!-- <div class="media">
-								<a href="#" class="pull-left">
-									<img src="" alt="" class="media-object">
+							@foreach ($status->replies as $reply)
+							<div class="media">
+								<a href="{{ route('profile.index', ['username' => $reply->user->username]) }}" class="pull-left">
+									<img src="{{ $reply->user->getAvatarUrl() }}" alt="{{ $reply->user->getNameOrUsername() }}" class="media-object">
 								</a>
 								<div class="media-body">
-									<h5 class="media-heading"><a href="#">Billy</a></h5>
-									<p>Indubidably</p>
+									<h5 class="media-heading"><a href="{{ route('profile.index', ['username' => $reply->user->username]) }}">{{ $reply->user->getNameOrUsername() }}</a></h5>
+									<p>{{ $reply->body }}</p>
 									<ul class="list-inline">
-										<li>8 minutes ago</li>
+										<li>{{ $reply->created_at->diffForHumans() }}</li>
 										<li><a href="#">Like</a></li>
 										<li>10 likes</li>
 									</ul>
 								</div>
-							</div> -->
+							</div>
+							@endforeach
 
 							<form role="form" action="{{ route('status.reply', ['statusId' => $status->id]) }}" method="post">
 								<div class="form-group{{ $errors->has("reply-{$status->id}") ? ' has-error' : ''}}">
